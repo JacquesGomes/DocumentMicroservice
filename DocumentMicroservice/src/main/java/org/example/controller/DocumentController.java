1b9db0a3cf00909f7f4bca6fa83adbf27f049947
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-08-29 16:02:31 -0300

    Ok

--- DocumentMicroservice/src/main/java/org/example/controller/DocumentController.java
+++ DocumentMicroservice/src/main/java/org/example/controller/DocumentController.java
@@ -10,6 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
+
+@CrossOrigin(origins = "*")
 @RestController
 public class DocumentController {
     private static final Logger log =
@@ -44,9 +48,10 @@ public class DocumentController {
         Document document = null;
 
         try {
-            log.info("Retrieving documento from the database, document id: "+ docid);
+            log.info("Retrieving document from the database, document id: "+ docid);
             document = documentRepository.findDocumentByDocId(docid);
             if(document == null || document.getDocId().equals("")){
+                log.error("The document doesnt exist on the database");
                 throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Document not found with docId: " + docid);
             } else {
                 log.info("Retrieved doc with success!");
@@ -58,6 +63,50 @@ public class DocumentController {
         }
 
         return document;
+    }
+
+// Assuming you have the necessary imports and annotations
+
+    @GetMapping("documents")
+    public List<Document> getAllDocuments() {
+        List<Document> documents;
+
+        try {
+            log.info("Retrieving all documents from the database");
+
+            // Using the documentRepository to fetch all documents
+            documents = documentRepository.findAll();
+
+            // Check if any documents are found
+            if (documents.isEmpty()) {
+                log.error("No documents found in the database");
+
+                // Throwing a ResponseStatusException with 503 (SERVICE_UNAVAILABLE) status
+                throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "No documents found in the database");
+            } else {
+                log.info("Retrieved documents with success!");
+            }
+        } catch (Exception e) {
+            log.error("Error retrieving documents", e);
+
+            // Throwing a ResponseStatusException with 503 (SERVICE_UNAVAILABLE) status
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Error retrieving documents");
+        }
+
+        // Returning the list of retrieved documents
+        return documents;
+    }
+
+
+    @GetMapping("documents/exist")
+    public boolean checkDocumentsExist(@RequestParam List<String> docids) {
+        for (String docid : docids) {
+            Document document = documentRepository.findDocumentByDocId(docid);
+            if (document == null || document.getDocId().equals("")) {
+                return false; // At least one document doesn't exist
+            }
+        }
+        return true; // All documents exist
     }
 
 }
