62d5406004251d1b811032c4747dee19071fde99
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-08-30 08:12:12 -0300

    Ok

--- DocumentMicroservice/src/main/java/org/example/controller/DocumentController.java
+++ DocumentMicroservice/src/main/java/org/example/controller/DocumentController.java
@@ -30,15 +30,11 @@ public class DocumentController {
     @PostMapping("document")
     public void storeDocument(@RequestBody Document document) {
         try{
-            log.info("Saving the document to the database: " + document.noteInformation());
             document = documentRepository.saveAndFlush(document);
             log.info("Document saved to the database: " + document.noteInformation());
         } catch(Exception e){
-            log.error("An error ocurred when saving the documento to the " +
-                    "database", e);
             throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "An error ocurred when saving the documento to the " +
                     "database");
-
         }
 
     }
@@ -48,16 +44,14 @@ public class DocumentController {
         Document document = null;
 
         try {
-            log.info("Retrieving document from the database, document id: "+ docid);
+
             document = documentRepository.findDocumentByDocId(docid);
             if(document == null || document.getDocId().equals("")){
-                log.error("The document doesnt exist on the database");
                 throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Document not found with docId: " + docid);
             } else {
                 log.info("Retrieved doc with success!");
             }
         } catch (Exception e){
-            log.error("Error retrieving the document", e);
             throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE,
                     "Error retrieving the document");
         }
@@ -65,38 +59,22 @@ public class DocumentController {
         return document;
     }
 
-// Assuming you have the necessary imports and annotations
-
     @GetMapping("documents")
     public List<Document> getAllDocuments() {
         List<Document> documents;
 
         try {
-            log.info("Retrieving all documents from the database");
-
-            // Using the documentRepository to fetch all documents
             documents = documentRepository.findAll();
-
-            // Check if any documents are found
             if (documents.isEmpty()) {
-                log.error("No documents found in the database");
-
-                // Throwing a ResponseStatusException with 503 (SERVICE_UNAVAILABLE) status
                 throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "No documents found in the database");
             } else {
                 log.info("Retrieved documents with success!");
             }
         } catch (Exception e) {
-            log.error("Error retrieving documents", e);
-
-            // Throwing a ResponseStatusException with 503 (SERVICE_UNAVAILABLE) status
             throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Error retrieving documents");
         }
-
-        // Returning the list of retrieved documents
         return documents;
     }
-
 
     @GetMapping("documents/exist")
     public boolean checkDocumentsExist(@RequestParam List<String> docids) {
